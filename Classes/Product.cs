8ea3a5727a5dbdada80c28ad61d9ce419af11270
Serhii Popil <sergii.popil@gmail.com>
2023-09-26 11:40:52 +0300

    add class Product

---
+++ Classes/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Shop.Classes
+{
+    public class Product
+    {
+        public const string Currency = "EUR";
+        public readonly DateTime Expiration = DateTime.Now.AddMonths(1);
+        public required int Id { get; set; }
+        public required string Title { get; set; }
+        public required int Count { get; set; }
+        public required decimal Price { get; set; }
+        public int Weight { get; set; }        
+        [SetsRequiredMembers]
+        public Product()
+        {
+        }
+    }
+}
