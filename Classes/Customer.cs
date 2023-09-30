60ef2666718c8f22e4f51aeacc1b73297e161bf4
Bohdan_Havryliuk <bogdangavrilyuk96@gmail.com>
2023-09-30 19:22:05 +0300

    Added Customer

---
+++ Classes/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Classes
+{
+    internal class Customer
+    {
+        private decimal _cash;
+        public decimal Cash
+        {
+            get
+            {
+                return _cash;
+            }
+            set
+            {
+                _cash = value;
+            }
+        }
+        public const int storeCard = 123456789;
+        public readonly string Name;
+        public required int NumberPhone { get; set; }
+        public Customer(string name)
+        {
+            Name = name;
+        }
+    }
+}
