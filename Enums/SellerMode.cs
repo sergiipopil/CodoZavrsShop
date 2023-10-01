9f5e2aa865307882d89d95764bda589e0e64d72c
Boichenko Sergei <boychenkosergeya@gmail.com>
2023-10-01 20:15:26 +0300

    Add classes Seller and SellerManager

--- Enums/SellerMode.cs
+++ Enums/SellerMode.cs
@@ -4,13 +4,7 @@
     {
         MainMenu = 0,
         SoldItem = 1,
-        AddItem = 2,
-        DeleteItem = 3,
-        GetAllItems = 4,
-        ItemDetailsById = 5,
-        ItemDetailsByTitle = 6,
-        DeleteExpirationProducts = 7,
-        OpenShop = 8,
-        CloseShop = 9
+        OpenShop = 2,
+        CloseShop = 3
     }
 }
