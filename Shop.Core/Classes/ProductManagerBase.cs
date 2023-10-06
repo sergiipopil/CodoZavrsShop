170ecafc0d310f7312e360528c04570429921f1e
Serhii Popil <sergii.popil@gmail.com>
2023-10-06 15:45:06 +0300

    fix add Shop.Core project

---
+++ Shop.Core/Classes/ProductManagerBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.Classes
+{
+    public class ProductManagerBase<T> where T : ProductBase
+    {
+        public void ShowMainProductInfo(ProductBase product)
+        {
+            Console.WriteLine($"ID:{product.Id}\tTitle:{product.Title}\tCount:{product.Count}\tPrice:{product.Price}\tProduction:{product.Production}");
+        }
+        public void ChangeProductPrice(ProductBase product, decimal price)
+        {
+            product.Price = price;
+        }
+    }
+}
