5a0e4757f856600a256eb68e3bf81b76412d9b02
Serhii Popil <sergii.popil@gmail.com>
2023-10-09 14:16:49 +0300

    add methods to LogService

--- Services/LogService.cs
+++ Services/LogService.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Shop.Services
 {
-    internal class LogService
+    public static class LogService
     {
+        private const string ExceptionLogFile = "..//..//../Logs/ExceptionLogs.txt";
+        public static void Log(Exception exception)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n============Exception==========\n");
+            Console.WriteLine($"{exception.Message}");
+            Console.WriteLine("\n============Exception==========\n");
+            Console.ResetColor();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{DateTime.Now}\r\n");
+            sb.Append($"Exception message:{exception.Message}\r\n");
+            if (!String.IsNullOrEmpty(exception.StackTrace))
+            {
+                sb.Append($"StackTrace:{exception.StackTrace}");
+            }
+            AddToFile(sb.ToString());
+        }
+        private static void AddToFile(string message)
+        {
+            using var file = new System.IO.StreamWriter(ExceptionLogFile, true);
+            file.WriteLine(message);
+            file.Close();
+        }
     }
 }
